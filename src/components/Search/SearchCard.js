cdba616a895b4e4f47ccb6260c03c4dc67a3791a
Kri <kritika.murugan@informatik.hs-fulda.de>
2021-12-22 13:32:17 +0100

    removed image

--- src/components/Search/SearchCard.js
+++ src/components/Search/SearchCard.js
@@ -1,9 +1,6 @@
 import React from "react"
-import catImage from "./catImage.jpeg"
 
 export default function SearchCard({tutor_last_name, tutor_first_name}) {
-  const imgFile="http://44.201.189.246:8000/imgTest.png"
-  const imgFile1="https://dummyimage.com/300"
     return (
         
         <div className="card">
@@ -12,7 +9,6 @@ export default function SearchCard({tutor_last_name, tutor_first_name}) {
     <h5 className="card-title">
         First Name: {tutor_first_name} </h5>
     <p className="card-text">LastName: {tutor_last_name}</p>
-    <img src={catImage} className="col-md-10 mb-2"/>
     <button className="btn btn-primary">Enroll</button>
   </div>
 </div>
