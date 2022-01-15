fbd69e04e263494bd200f039197ae73e1d554609
Dipesh Chaudhary <dipesh.chaudhary@informatk.hs-fulda.de>
2022-01-15 21:05:24 +0100

    minor change in api end point

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-<<<<<<< HEAD
 import Navbar from './Navbar/FuldemyNavbar';
-=======
-import Navbar from './Navbar/Navbar';
->>>>>>> 0ae55ebbbd570fec75be45224fd09b9027efe5b6
 import AddTutor from './components/Tutor/AddTutor';
 import DeleteTutor from './components/Tutor/DeleteTutor';
 import GetByIdTutor from './components/Tutor/GetById';
 import UpdateTutor from './components/Tutor/Update';
 import GetAll from './components/Tutor/GetAll';
 import Home from './Home/Home';
-<<<<<<< HEAD
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import TutorDetail from './components/TutorDetail/TutorDetail';
@@ -34,28 +29,11 @@ function App() {
       
     <Routes>
        <Route path="/" element={<Home />} />
-=======
-
-
-function App() {
-  return (
-    <div className="App">
-<BrowserRouter>
-<h6>Fulda University of Applied Sciences Software Engineering Project, Fall 2021 For
-Demonstration Only</h6>
-<Navbar />
-
-      <div className="mt-5 container">
-        <div className="card p-5">
-    <Routes>
-      <Route path="/" element={<Home />} />
->>>>>>> 0ae55ebbbd570fec75be45224fd09b9027efe5b6
       <Route path="/add-tutor" element={<AddTutor />} />
       <Route path="/delete" element={<DeleteTutor />} />
       <Route path="/get-by-id" element={<GetByIdTutor />} />
       <Route path="/update" element={<UpdateTutor />} />
       <Route path="/update/:id" element={<UpdateTutor />} />
-<<<<<<< HEAD
       <Route path="/get-all" element={<GetAll />} /> 
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Signup />} />
@@ -65,25 +43,9 @@ Demonstration Only</h6>
       <Route path="/messages" element={<ChatBody />} />
     </Routes>
    
-=======
-      <Route path="/get-all" element={<GetAll />} />
-    </Routes>
-    </div>
-      </div>
-      <h6>Fulda University of Applied Sciences Software Engineering Project, Fall 2021 For
-Demonstration Only</h6>
->>>>>>> 0ae55ebbbd570fec75be45224fd09b9027efe5b6
   </BrowserRouter>
-
-      
-     
-         
         
     </div>
-<<<<<<< HEAD
-=======
-    
->>>>>>> 0ae55ebbbd570fec75be45224fd09b9027efe5b6
   );
 }
 
